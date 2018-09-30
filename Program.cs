9c3497cc9a11b88cfc509cbf38d8252e7204b197
Daniel Mendes <danielstunna06@gmail.com>
2018-09-30 21:35:38 +0100

    Raiz Quandrada de 15 Numeros

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade33
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i,elemento;
+			double [] v = new double [15];
+
+			for (i=0;i<15;i++)
+			{
+				Console.WriteLine("Digite um numero para v["+i+"] :");
+				elemento = Convert.ToInt32(Console.ReadLine());
+
+				if (elemento<0)
+				{
+					v[i] = -1;
+				} else
+				{
+					v[i] = Math.Sqrt(elemento);
+				}
+			}
+
+			for (i = 0; i < 15; i++)
+			{
+				Console.WriteLine("v[" + i + "] = " + v[i]);
+			}
+				Console.Read();
+
+		}
+	}
+}
